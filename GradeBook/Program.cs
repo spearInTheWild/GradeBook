ff13e985225d07dc0b85a1108112b5a779abc8f6
Matthew Spear <mspear@enetpartners.com>
2019-05-19 20:05:17 -0400

    First Commit

---
+++ GradeBook/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            var name = Console.ReadLine();
+            Console.WriteLine($"Hello {name}");
+        }
+    }
+}
